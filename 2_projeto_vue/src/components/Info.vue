06077c795473c67cf7f1d6012143e5b6a7517843
DaniloArr <danilo.arreb@gmail.com>
2023-05-06 15:46:43 -0300

    Diretivas

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,16 +1,25 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+        <p v-else>Não estou trabalhando no momento</p>
         <p>Utilizo as seguintes linguagens</p>
         <ul>
             <li>JavaScript</li>
             <li>Java</li>
             <li>MySQL</li>
         </ul>
+        <p v-show="mostrar_email">Email para qualquer dúvida: {{ email }}</p>
     </div>
 </template>
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                esta_trabalhando: false,
+                mostrar_email: true,
+                email: 'bythewhei@gmail.com'
+            }
+        }
     }
 </script>
