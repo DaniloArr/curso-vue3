e336d6f6049cff8b664aaba64d5d71a477732366
DaniloArr <danilo.arreb@gmail.com>
2023-05-09 16:16:02 -0300

    atributos dinâmicos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>MySQL</li>
         </ul>
         <p v-show="mostrar_email">Email para qualquer dúvida: {{ email }}</p>
+        <p>Acesse o meu Github <a v-bind:href="meu_link" target="_blank">clique aqui!</a> </p>
+        <Picture />
     </div>
 </template>
 <script>
-    export default {
-        name: 'Info',
-        data() {
-            return {
-                esta_trabalhando: false,
-                mostrar_email: true,
-                email: 'bythewhei@gmail.com'
-            }
+import Picture from './Picture.vue'
+export default {
+    components: { 
+     Picture 
+    },
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: 'bythewhei@gmail.com',
+            meu_link: 'https://github.com/DaniloArr'
         }
     }
+}
 </script>
---
+++ 2_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,14 @@
+<template>
+    <img :src="avatar" :alt="descricao" style="width: 250px;">
+</template>
+<script>
+export default {
+    name: 'Picture',
+    data() {
+        return{
+            avatar: "/img/carangueijo.jpg",
+            descricao: "Seu Sirigueijo"
+        }
+    }
+}
+</script>
