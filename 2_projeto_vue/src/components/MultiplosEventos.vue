4d1786dee2016bedc37b9ac42721b13ff01649e3
DaniloArr <danilo.arreb@gmail.com>
2023-05-10 17:34:44 -0300

    multiplos eventos

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="primeiro($event), segundo(txt_segundo, $event), terceiro(), quarto($event), quinto()">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return {
+            multiplos_eventos: "",
+            txt_segundo: "Não to brincando "
+        }
+    },
+    methods: {
+        primeiro (e){
+            this.multiplos_eventos = "Vou sumir !!!"
+        },
+        segundo(txt, e) {
+            setTimeout(() => this.multiplos_eventos = txt, 500)
+        },
+        terceiro() {
+            setTimeout(() => this.multiplos_eventos = "", 1500)
+        },
+        quarto(e) {
+            setTimeout(() => this.multiplos_eventos = "Eu avisei", 3000)
+        },
+        quinto() {
+            setTimeout(() => this.multiplos_eventos = "", 4000)
+        },
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 85px;
+
+    }
+</style>
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,12 +2,18 @@
     <div>
         <h1>Olá Danilo</h1>
         <p>Meu nome artístico {{ nome }} e tenho como profissão {{ profissao }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
+
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data(){
             return {
                 nome: "Danilin",
