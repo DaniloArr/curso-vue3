57fa072204813fe07b93a52fc223ad60e8754646
DaniloArr <danilo.arreb@gmail.com>
2023-05-10 17:45:19 -0300

    Reutilização de componentes

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,22 +1,26 @@
 <template>
     <div>
+        <reutilizacao />
         <h1>Olá Danilo</h1>
         <p>Meu nome artístico {{ nome }} e tenho como profissão {{ profissao }}</p>
         <MultiplosEventos />
+        <reutilizacao />
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue'
+import Reutilizacao from './Reutilizacao.vue'
 
     export default {
         name: 'PrimeiroComponente',
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+                Reutilizacao
         },
         data(){
             return {
-                nome: "Danilin",
+                nome: "danilindosolo",
                 profissao: "Garoto de Programa"
             }
         }
---
+++ 2_projeto_vue/src/components/Reutilizacao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="ativar">Click here !</button>
+        <span v-show="ativou"> &nbsp; Botão ativado</span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Reutilizacao',
+    data() {
+        return {
+            ativou: false
+        }
+    },
+    methods: {
+        ativar() {
+            this.ativou = !this.ativou;
+        }
+    }
+}
+</script>
