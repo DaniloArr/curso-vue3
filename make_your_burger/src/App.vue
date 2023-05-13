7a8b803d07d1cd12f66f0c84dcf73f41e98aabb3
DaniloArr <danilo.arreb@gmail.com>
2023-05-13 17:37:59 -0300

    Criando o banner

--- make_your_burger/src/App.vue
+++ make_your_burger/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar :logo="logo_src" :alt="app_name"/>
     <router-view />
     <Footer />
   </div>
@@ -15,9 +15,32 @@ import FooterVue from './components/Footer.vue';
     components: {
       Navbar,
       Footer
+    },
+    data() {
+      return {
+        logo_src: "/img/logo.png",
+        app_name: "Make your burger"
+      }
     }
   }
 </script>
 <style>
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
